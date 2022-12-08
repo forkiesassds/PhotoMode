c16f2d003f68e7719a51bfccc32c72f0430ef27c
forkiesassds <icanttellyou@memeware.net>
2022-12-08 17:51:33 +0200

    remove unused imports

--- forge/src/main/java/me/icanttellyou/mods/photomode/forge/PhotoMode.java
+++ forge/src/main/java/me/icanttellyou/mods/photomode/forge/PhotoMode.java
@@ -1,6 +1,5 @@
 package me.icanttellyou.mods.photomode.forge;
 
-//import dev.architectury.hooks.client.screen.ScreenHooks;
 import me.icanttellyou.mods.photomode.common.client.PhotoModeScreen;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.GameMenuScreen;
@@ -10,9 +9,6 @@ import net.minecraft.text.Text;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
-
-import java.lang.reflect.InvocationTargetException;
 
 @Mod("photomode")
 public class PhotoMode {
