d3c2cc8faaf3791df632978f39d29efdb3734b6f
forkiesassds <icanttellyou@memeware.net>
2022-12-20 16:33:00 +0200

    fix photo mode button not appearing on nt's old pause screen

--- forge/src/main/java/me/icanttellyou/mods/photomode/forge/PhotoMode.java
+++ forge/src/main/java/me/icanttellyou/mods/photomode/forge/PhotoMode.java
@@ -11,16 +11,21 @@ import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.List;
+
 @Mod("photomode")
 public class PhotoMode {
     MinecraftClient client = MinecraftClient.getInstance();
+    private static final List<String> usePauseScreenWorkarround = List.of(
+        "NostalgicPauseScreen" //used by nt 2.0 old pause screen tweak
+    );
     public PhotoMode() {
         MinecraftForge.EVENT_BUS.addListener(this::screenEventHandler);
     }
 
     private void screenEventHandler(ScreenEvent event) {
         Screen screen = event.getScreen();
-        if (screen instanceof GameMenuScreen && event instanceof ScreenEvent.InitScreenEvent) {
+        if ((screen instanceof GameMenuScreen || usePauseScreenWorkarround.contains(screen.getClass().getSimpleName())) && event instanceof ScreenEvent.InitScreenEvent) {
             screen.addDrawableChild(new ButtonWidget(screen.width / 2 - 48, 8, 98, 20, new TranslatableText("gui.photomode"), (button) -> {
                 client.setScreen(new PhotoModeScreen(Text.of("")));
             }));
