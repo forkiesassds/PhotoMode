9a74470dc05c3eebc4f0a7878715d30299fa527d
forkiesassds <icanttellyou@memeware.net>
2023-08-21 04:23:58 +0300

    fix sodium 0.5.2 crashes

--- common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/compat/MixinDefaultChunkRenderer.java
+++ common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/compat/MixinDefaultChunkRenderer.java
@@ -2,17 +2,17 @@ package me.icanttellyou.mods.photomode.common.mixin.compat;
 
 import me.icanttellyou.mods.photomode.common.client.PhotoModeScreen;
 import me.jellysquid.mods.sodium.client.model.quad.properties.ModelQuadFacing;
-import me.jellysquid.mods.sodium.client.render.chunk.RenderSectionManager;
+import me.jellysquid.mods.sodium.client.render.chunk.DefaultChunkRenderer;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(RenderSectionManager.class)
-public class MixinRenderSectionManager {
+@Mixin(DefaultChunkRenderer.class)
+public class MixinDefaultChunkRenderer {
     @Inject(method = "getVisibleFaces", at = @At("HEAD"), remap = false, cancellable = true)
-    private void photoMode$sodium05x$getVisibleFaces(int x, int y, int z, CallbackInfoReturnable<Integer> cir) {
+    private static void photoMode$sodium05x$getVisibleFaces(int originX, int originY, int originZ, int chunkX, int chunkY, int chunkZ, CallbackInfoReturnable<Integer> cir) {
         if (MinecraftClient.getInstance().currentScreen instanceof PhotoModeScreen)
             cir.setReturnValue(ModelQuadFacing.ALL);
     }
