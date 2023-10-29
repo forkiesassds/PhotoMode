b61d4a9990e6a523ba7d0aeb4800891041f48561
forkiesassds <icanttellyou@memeware.net>
2023-10-29 21:27:44 +0200

    implement fix for hud still being drawn

--- common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinGameRenderer.java
+++ common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinGameRenderer.java
@@ -2,18 +2,23 @@ package me.icanttellyou.mods.photomode.common.mixin;
 
 import me.icanttellyou.mods.photomode.common.client.PhotoModeScreen;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GameRenderer.class)
 public abstract class MixinGameRenderer {
+    @Unique
     MinecraftClient client = MinecraftClient.getInstance();
+    @Unique
     float delta = 0.0F;
 
     @Inject(method = "bobView", at = @At("HEAD"), cancellable = true)
@@ -38,5 +43,10 @@ public abstract class MixinGameRenderer {
                     .setOrtho(-width, width, -height, height, ((GameRenderer)(Object)this).method_32796() * -2.0f, ((GameRenderer)(Object)this).method_32796() * 2.0f)
                     .translate(((PhotoModeScreen) client.currentScreen).getPanX(delta), -((PhotoModeScreen) client.currentScreen).getPanY(delta), 0.0F)));
         }
+    }
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;render(Lnet/minecraft/client/util/math/MatrixStack;F)V"))
+    private void photoMode$forceHideHud(InGameHud instance, MatrixStack stack, float tickDelta) {
+        if (!(client.currentScreen instanceof PhotoModeScreen)) instance.render(stack, tickDelta);
     }
 }
--- common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinWorldRenderer.java
+++ common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinWorldRenderer.java
@@ -4,12 +4,14 @@ import me.icanttellyou.mods.photomode.common.client.PhotoModeScreen;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.WorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class MixinWorldRenderer {
+    @Unique
     MinecraftClient client = MinecraftClient.getInstance();
 
     @Inject(method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At("HEAD"), cancellable = true)
