004a82a6d996f73a478601cd01504e63d7abd201
icanttellyou <54495659+forkiesassds@users.noreply.github.com>
2024-04-23 17:03:28 +0300

    Backport #17 to 1.19.3
    (cherry picked from commit e42ae494843c0d611956b3c46cad75a26d8b75ae)

--- common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinFrustum.java
+++ common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinFrustum.java
@@ -4,27 +4,25 @@ import me.icanttellyou.mods.photomode.common.client.PhotoModeScreen;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Frustum;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Frustum.class)
 public class MixinFrustum {
-    //this kills frustum culling to fix an issue where the game hangs due to it trying to calculate the frustum intersections
-    //bad hack for a bad problem, PLEASE REPLACE THIS WITH A BETTER SOLUTION!!!!!!!!!!!
-    @Inject(method = "isVisible(DDDDDD)Z", at = @At("HEAD"), cancellable = true)
-    private void injectIsVisible(CallbackInfoReturnable<Boolean> cir) {
-        if(MinecraftClient.getInstance().currentScreen instanceof PhotoModeScreen) {
-            cir.setReturnValue(true);
-            cir.cancel();
-        }
-    }
+    @Unique
+    MinecraftClient photoMode$client = MinecraftClient.getInstance();
 
     @Inject(method = "method_38557", at = @At("HEAD"), cancellable = true)
-    private void injectMethod38557(CallbackInfoReturnable<Frustum> cir) {
-        if(MinecraftClient.getInstance().currentScreen instanceof PhotoModeScreen) {
-            cir.setReturnValue((Frustum)((Object)this));
-            cir.cancel();
+    private void photoMode$coverBoxAroundSetPosition(CallbackInfoReturnable<Frustum> cir) {
+        if (photoMode$client.currentScreen instanceof PhotoModeScreen) {
+            float div = (float) Math.pow(2.0, ((PhotoModeScreen) this.photoMode$client.currentScreen).getZoom(photoMode$client.getTickDelta()));
+            float width = photoMode$client.getWindow().getFramebufferWidth() / div;
+            float height = photoMode$client.getWindow().getFramebufferHeight() / div;
+            if (width <= 10 || height <= 10) {
+                cir.setReturnValue((Frustum) ((Object) this));
+            }
         }
     }
 }
