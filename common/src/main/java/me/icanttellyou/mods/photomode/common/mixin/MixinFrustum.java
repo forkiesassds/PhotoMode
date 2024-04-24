fb3e85268646972a9d467631a08ec3fee846a0f5
forkiesassds <icanttellyou@memeware.net>
2024-04-24 16:02:28 +0300

    fix fog not respecting framerate

--- common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinFrustum.java
+++ common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinFrustum.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Frustum.class)
-public class MixinFrustum {
+public abstract class MixinFrustum {
     @Unique
     MinecraftClient photoMode$client = MinecraftClient.getInstance();
 
--- common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinWorldRenderer.java
+++ common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinWorldRenderer.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
-public class MixinWorldRenderer {
+public abstract class MixinWorldRenderer {
     @Unique
     MinecraftClient client = MinecraftClient.getInstance();
 
