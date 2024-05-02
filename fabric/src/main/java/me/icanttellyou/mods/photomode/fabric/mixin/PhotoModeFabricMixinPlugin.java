4d8ece97a28383ce824749c9d7255849626989f0
forkiesassds <icanttellyou@memeware.net>
2024-05-02 17:04:56 +0300

    fix mixin errors

--- fabric/src/main/java/me/icanttellyou/mods/photomode/fabric/mixin/PhotoModeFabricMixinPlugin.java
+++ fabric/src/main/java/me/icanttellyou/mods/photomode/fabric/mixin/PhotoModeFabricMixinPlugin.java
@@ -1,38 +1,22 @@
-package me.icanttellyou.mods.photomode.common.mixin;
+package me.icanttellyou.mods.photomode.fabric.mixin;
 
 import com.google.common.collect.ImmutableMap;
-import me.icanttellyou.mods.photomode.common.PhotoModeCommon;
+import net.fabricmc.loader.api.FabricLoader;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
-import org.spongepowered.asm.service.MixinService;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Supplier;
 
-public class PhotoModeMixinPlugin implements IMixinConfigPlugin {
-    public static boolean HAS_SODIUM;
-
-    static {
-        try {
-            MixinService.getService().getBytecodeProvider().getClassNode("me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer");
-            HAS_SODIUM = true;
-        } catch (Throwable t) {
-            HAS_SODIUM = false;
-        }
-
-        if (HAS_SODIUM)
-            PhotoModeCommon.LOGGER.info("[PhotoMode] Detected Sodium or it's forks!");
-    }
-
+public class PhotoModeFabricMixinPlugin implements IMixinConfigPlugin {
     private static final Supplier<Boolean> TRUE = () -> true;
 
     private static final Map<String, Supplier<Boolean>> CONDITIONS = ImmutableMap.of(
-        "me.icanttellyou.mods.photomode.common.mixin.MixinWorldRendererVanilla", () -> !HAS_SODIUM,
-        "me.icanttellyou.mods.photomode.common.mixin.compat.MixinRegionChunkRenderer", () -> HAS_SODIUM,
-        "me.icanttellyou.mods.photomode.common.mixin.compat.MixinSodiumWorldRenderer", () -> HAS_SODIUM
+        "me.icanttellyou.mods.photomode.fabric.mixin.JsonEffectShaderProgram", () -> !(FabricLoader.getInstance().isModLoaded("satin")
+                        || FabricLoader.getInstance().isModLoaded("architectury"))
     );
 
     @Override
