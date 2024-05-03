4a666637d2299da44fd6330bc517e000742e6b3a
forkiesassds <icanttellyou@memeware.net>
2024-05-03 15:22:03 +0300

    fix depth in fabulous graphics

--- common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinWorldRenderer.java
+++ common/src/main/java/me/icanttellyou/mods/photomode/common/mixin/MixinWorldRenderer.java
@@ -1,12 +1,15 @@
 package me.icanttellyou.mods.photomode.common.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import me.icanttellyou.mods.photomode.common.client.PhotoModeScreen;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.PostEffectProcessor;
 import net.minecraft.client.render.WorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
@@ -22,5 +25,17 @@ public abstract class MixinWorldRenderer {
     @Inject(method = "renderClouds", at = @At("HEAD"), cancellable = true)
     public void injectRenderClouds(CallbackInfo info) {
         if (client.currentScreen instanceof PhotoModeScreen) info.cancel();
+    }
+
+    /**
+     * Fixes depth buffer being messed up in fabulous graphics... Somewhat, as depth information is not present for translucent surfaces.
+     * @param instance Effect processor
+     * @param tickDelta Tick delta
+     */
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/PostEffectProcessor;render(F)V", ordinal = 1))
+    private void photoMode$fixFabulousDepthSomewhat(PostEffectProcessor instance, float tickDelta) {
+        RenderSystem.depthMask(false);
+        instance.render(tickDelta);
+        RenderSystem.depthMask(true);
     }
 }
