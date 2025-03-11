882830d59c680a2a6d9624ca94544e0588d01c84
forkiesassds <icanttellyou@memeware.net>
2025-03-11 18:30:17 +0200

    1.21.5-pre1

--- common/src/main/java/me/icanttellyou/mods/photomode/mixin/MixinPostEffectPass.java
+++ common/src/main/java/me/icanttellyou/mods/photomode/mixin/MixinPostEffectPass.java
@@ -1,11 +1,11 @@
 package me.icanttellyou.mods.photomode.mixin;
 
+import com.mojang.blaze3d.buffers.GpuBuffer;
+import com.mojang.blaze3d.systems.RenderPass;
 import com.mojang.blaze3d.systems.RenderSystem;
 import me.icanttellyou.mods.photomode.client.PhotoModeScreen;
-import net.minecraft.class_10883;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.Framebuffer;
-import net.minecraft.client.gl.GpuBuffer;
 import net.minecraft.client.gl.PostEffectPass;
 import net.minecraft.client.util.Handle;
 import net.minecraft.util.Identifier;
@@ -21,20 +21,21 @@ import java.util.function.Consumer;
 
 @Mixin(PostEffectPass.class)
 public abstract class MixinPostEffectPass {
-    @Inject(method = "method_67884", at = @At(value = "INVOKE", target = "Lnet/minecraft/class_10883;method_68416(Ljava/lang/String;[F)V"), locals = LocalCapture.CAPTURE_FAILHARD)
+    @Inject(method = "method_67884", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderPass;drawIndexed(II)V"), locals = LocalCapture.CAPTURE_FAILHARD)
     private void photoMode$setupShaderUniform(
             Handle<Framebuffer> handle,
             Matrix4f matrix4f,
             Map<Identifier, Handle<Framebuffer>> map,
-            Consumer<class_10883> consumer,
+            Consumer<RenderPass> consumer,
             CallbackInfo ci,
             Framebuffer framebuffer,
             GpuBuffer gpuBuffer,
             RenderSystem.ShapeIndexBuffer shapeIndexBuffer,
-            class_10883 lv
+            GpuBuffer gpuBuffer2,
+            RenderPass renderPass
     ) {
         if (MinecraftClient.getInstance().currentScreen instanceof PhotoModeScreen pmScreen) {
-            lv.method_68416("Intensity", pmScreen.shaderIntensity);
+            renderPass.setUniform("Intensity", pmScreen.shaderIntensity);
         }
     }
 }
