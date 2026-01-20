80f777d65e9b4d5f9553213416dcabc4394d6c93
icanttellyou <cuboidedposition32@protonmail.com>
2026-01-20 14:23:18 +0200

    feat: Add dimension disable list

--- src/main/java/mod/icanttellyou/picturemode/mixin/PauseScreenMixin.java
+++ src/main/java/mod/icanttellyou/picturemode/mixin/PauseScreenMixin.java
@@ -4,7 +4,11 @@ import mod.icanttellyou.picturemode.client.gui.PictureModeScreen;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,12 +22,17 @@ public abstract class PauseScreenMixin extends Screen {
 
     @Inject(method = "createPauseMenu", at = @At("TAIL"))
     private void addPMButtonToPauseMenu(CallbackInfo ci) {
-        this.addRenderableWidget(
-            Button.builder(Component.translatable("gui.picturemode"), button ->
-                minecraft.setScreen(new PictureModeScreen(this, Component.literal(""))))
+        Button pmButton = Button.builder(Component.translatable("gui.picturemode"), button ->
+                this.minecraft.setScreen(new PictureModeScreen(this, Component.literal(""))))
             .pos(width / 2 - 48, 8)
             .width(98)
-            .build()
-        );
+            .build();
+
+        Level level = this.minecraft.level;
+        boolean disabled = level.dimensionTypeRegistration().is(TagKey.create(Registries.DIMENSION_TYPE,
+                Identifier.parse("picturemode:disabled")));
+
+        pmButton.active = !disabled;
+        this.addRenderableWidget(pmButton);
     }
 }
