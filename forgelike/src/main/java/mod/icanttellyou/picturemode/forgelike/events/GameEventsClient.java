f57c834630e71bc5f35cc5a8fc8432a2ce151396
icanttellyou <cuboidedposition32@protonmail.com>
2026-01-22 12:19:10 +0200

    fix: IntelliJ you're stupid

--- forgelike/src/main/java/mod/icanttellyou/picturemode/forgelike/events/GameEventsClient.java
+++ forgelike/src/main/java/mod/icanttellyou/picturemode/forgelike/events/GameEventsClient.java
@@ -13,8 +13,8 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 //? if <1.20.5
 //import net.neoforged.neoforge.event.TickEvent
-//? if >=1.21.6
 import net.neoforged.neoforge.client.event.ScreenEvent;
+//? if >=1.21.6
 import net.neoforged.neoforge.client.event.ViewportEvent;
 import net.neoforged.neoforge.event.level.LevelEvent;
 //? } else {
