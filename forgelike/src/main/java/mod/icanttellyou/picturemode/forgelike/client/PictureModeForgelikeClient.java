651d94c91bcfef6a8603c2d0191957b7db6ba59e
icanttellyou <cuboidedposition32@protonmail.com>
2026-02-23 10:31:30 +0200

    fix: Forgot to add import

--- forgelike/src/main/java/mod/icanttellyou/picturemode/forgelike/client/PictureModeForgelikeClient.java
+++ forgelike/src/main/java/mod/icanttellyou/picturemode/forgelike/client/PictureModeForgelikeClient.java
@@ -19,6 +19,7 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 //? } else {
 /*import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
