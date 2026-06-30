0b44b0df6fd8856199c6ead2385a6b4081411a35
icanttellyou <cuboidedposition32@protonmail.com>
2026-07-01 01:57:08 +0300

    port: 26.3-snapshot-2

--- src/main/java/mod/icanttellyou/picturemode/client/render/shader/ShaderUtil.java
+++ src/main/java/mod/icanttellyou/picturemode/client/render/shader/ShaderUtil.java
@@ -51,7 +51,11 @@ public class ShaderUtil {
     public static boolean useShaderTransparency() {
         //? if >=26.2 {
         /*Minecraft minecraft = Minecraft.getInstance();
+        //? if >=26.3 {
+        /^return minecraft.gameRenderer.useImprovedTransparency();
+        ^///? } else {
         return minecraft.gameRenderer.gameRenderState().useShaderTransparency();
+        //? }
         *///? } else {
         return Minecraft.useShaderTransparency();
         //? }
