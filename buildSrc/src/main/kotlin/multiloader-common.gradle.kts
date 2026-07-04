6661f038518c973d0ae702b9a902ab0cfce24e5b
icanttellyou <cuboidedposition32@protonmail.com>
2026-07-05 01:28:44 +0300

    build: Remove Maven Central for YACL

--- buildSrc/src/main/kotlin/multiloader-common.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader-common.gradle.kts
@@ -47,7 +47,6 @@ repositories {
     }
     exclusiveContent {
         forRepositories(
-            mavenCentral(), //1.21.1+ is on Maven Central
             maven("https://maven.isxander.dev/releases") { name = "Xander Maven" }
         )
         filter { includeGroupAndSubgroups("dev.isxander") }
